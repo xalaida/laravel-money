642be41b8b42d2caa9e21aaf639470e0d4856b3d
nevadskiy <nevadskiy@gmail.com>
2020-09-20 22:59:22 +0300

    Add uuid key to currency

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -7,12 +7,15 @@ namespace Jeka\Money\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Nevadskiy\Uuid\Uuid;
 
 /**
+ * @property string id
  * @property Carbon created_at
  * @property Carbon updated_at
  */
 class Currency extends Model
 {
-    use HasFactory;
+    use HasFactory,
+        Uuid;
 }
