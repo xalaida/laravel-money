0860dfe05a6354ab558c4824678048e3edd0c508
nevadskiy <nevadskiy@gmail.com>
2020-09-21 20:04:26 +0300

    Add currency seeder

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jeka\Money\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Jeka\Money\Models\Currency;
 
 class CurrencySeeder extends Seeder
 {
@@ -13,6 +14,31 @@ class CurrencySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Currency::query()->truncate();
+
+        foreach ($this->currencies() as $category) {
+            Currency::query()->create($category);
+        }
+    }
+
+    /**
+     * Default currencies to be seeded.
+     * TODO: refactor using config or CLI command for seeding
+     * TODO: extract currencies into resources/currencies.php file
+     *
+     * @return string[]
+     */
+    protected function currencies(): array
+    {
+        return [
+            [
+                'code' => 'USD',
+                'precision' => 2,
+            ],
+            [
+                'code' => 'EUR',
+                'precision' => 2,
+            ],
+        ];
     }
 }
