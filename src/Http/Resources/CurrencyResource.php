cc93aefd27f4960ee85918bd92c55cd10c6d6aac
nevadskiy <nevadskiy@gmail.com>
2020-10-08 21:39:45 +0300

    Refactor using resource

---
+++ src/Http/Resources/CurrencyResource.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jeka\Money\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Jeka\Money\Models\Currency;
+
+/**
+ * @mixin Currency
+ */
+class CurrencyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param $request
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'name' => $this->name,
+            'symbol' => $this->symbol,
+        ];
+    }
+}
---
+++ src/Http/Resources/MoneyResource.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jeka\Money\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Jeka\Money\Money;
+
+/**
+ * @mixin Money
+ */
+class MoneyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param $request
+     */
+    public function toArray($request): array
+    {
+        return [
+            'amount' => $this->getAmount(),
+            'formatted' => $this->format(),
+            'currency' => CurrencyResource::make($this->getCurrency()),
+        ];
+    }
+}
