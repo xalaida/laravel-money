6890e8f2c602fcbcda1fbc424419225c4ee6a3f0
nevadskiy <nevadskiy@gmail.com>
2021-04-11 21:54:47 +0300

    Simplify expression

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -60,7 +60,7 @@ class Currency extends Model
      */
     private function assertPositiveRate(float $rate): void
     {
-        if ($rate < 0 || 0 === $rate) {
+        if ($rate <= 0) {
             throw new InvalidRateException();
         }
     }
