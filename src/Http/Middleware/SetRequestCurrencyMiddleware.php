3b4fbc7a4f20496f756e7255f90f3821b77b5c63
nevadskiy <nevadskiy@gmail.com>
2021-11-03 14:07:32 +0200

    Rename middleware

--- src/Http/Middleware/SetRequestCurrencyMiddleware.php
+++ src/Http/Middleware/SetRequestCurrencyMiddleware.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Nevadskiy\Money\Models\Currency;
 use Nevadskiy\Money\Queries\CurrencyQuery;
 
-final class SetCurrencyMiddleware
+final class SetRequestCurrencyMiddleware
 {
     /**
      * The currency query instance.
