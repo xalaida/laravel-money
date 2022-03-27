8d60bbe6c74622f50118fc1a7b859875fde56c70
Nevadskiy <nevadskiy@gmail.com>
2022-03-27 14:45:50 +0300

    Refactor

--- src/Console/UpdateCurrencyRatesCommand.php
+++ src/Console/UpdateCurrencyRatesCommand.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Database\Eloquent\Collection;
 use Nevadskiy\Money\Events\CurrencyRateUpdated;
 use Nevadskiy\Money\Models\Currency;
+use Nevadskiy\Money\Models\CurrencyResolver;
 use Nevadskiy\Money\RateProvider\RateProvider;
 use Nevadskiy\Money\ValueObjects\Rate;
 
@@ -70,7 +71,9 @@ class UpdateCurrencyRatesCommand extends Command
      */
     protected function currencies(array $rates): Collection
     {
-        return Currency::query()
+        return CurrencyResolver::resolve()
+            ->newQuery()
+            // TODO: refactor array_keys to use another format. introduce better provider rates structure.
             ->whereIn('code', array_keys($rates))
             ->get();
     }
