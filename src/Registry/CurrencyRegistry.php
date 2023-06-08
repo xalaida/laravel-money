bafb05d568e972512ef16720f14b0e94c040654d
nevadskiy <nevadskiy@gmail.com>
2023-06-09 00:46:17 +0300

    Add currency registry

---
+++ src/Registry/CurrencyRegistry.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nevadskiy\Money\Registry;
+
+class CurrencyRegistry
+{
+    /**
+     * The registered currency list.
+     *
+     * @var array
+     */
+    protected $currencies = [];
+
+    /**
+     * Register the given currency.
+     */
+    public function set(string $currency, array $options): void
+    {
+        $this->currencies[$currency] = $options;
+    }
+
+    /**
+     * Get options of the currency.
+     */
+    public function get(string $currency): array
+    {
+        return $this->currencies[$currency];
+    }
+}
