3516a99718d41d64129b29a2559035e19dc4d95e
nevadskiy <nevadskiy@gmail.com>
2023-06-11 02:05:37 +0300

    Simplify model

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -5,31 +5,20 @@ namespace Nevadskiy\Money\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-use Nevadskiy\Money\Casts\AsRate;
 use Nevadskiy\Money\Events;
-use Nevadskiy\Money\ValueObjects\Rate;
 
 /**
  * @property int id
  * @property string code
  * @property string name
  * @property string symbol
- * @property int precision
- * @property Rate rate
+ * @property int scale
+ * @property float rate
  * @property Carbon created_at
  * @property Carbon updated_at
  */
 class Currency extends Model
 {
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'rate' => AsRate::class,
-    ];
-
     /**
      * The event map for the model.
      *
@@ -58,10 +47,10 @@ class Currency extends Model
     }
 
     /**
-     * Get the major unit multiplier according to the precision.
+     * Get the major unit multiplier according to the scale.
      */
     public function getMajorMultiplier(): int
     {
-        return 10 ** $this->precision;
+        return 10 ** $this->scale;
     }
 }
