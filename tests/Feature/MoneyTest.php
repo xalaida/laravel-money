fcc33e472dd855d6349fcd915df308f19cbde98c
nevadskiy <nevadskiy@gmail.com>
2023-06-11 19:57:02 +0300

    Split tests

--- tests/Feature/MoneyTest.php
+++ tests/Feature/MoneyTest.php
@@ -22,28 +22,7 @@ class MoneyTest extends TestCase
         static::assertSame(100, $original->getAmount());
     }
 
-    public function test_it_can_be_formatted_to_string_according_to_current_locale(): void
-    {
-        static::assertSame('$1.00', (new Money(100, 'USD'))->format());
-    }
-
-    public function test_it_can_be_formatted_to_string_according_to_given_locale(): void
-    {
-        $money = new Money(100, 'UAH');
-
-        $this->app->setLocale('uk');
-
-        static::assertSame("1,00 ₴", $money->format());
-    }
-
-    public function test_it_can_be_formatted_according_to_the_given_locale(): void
-    {
-        $money = new Money(100, 'UAH');
-
-        static::assertSame("1,00 ₴", $money->format('uk'));
-    }
-
-    public function test_it_can_determine_major_units_amount(): void
+    public function test_it_returns_major_units_amount(): void
     {
         $this->app->instance(Scaler::class, new ArrayScaler(['ABC' => 3]));
 
@@ -52,18 +31,13 @@ class MoneyTest extends TestCase
         static::assertSame(3, $money->getMajorUnits());
     }
 
-    public function test_it_calculates_major_units_correctly_if_scale_is_zero(): void
+    public function test_it_returns_major_units_correctly_if_scale_is_zero(): void
     {
         $this->app->instance(Scaler::class, new ArrayScaler(['ABC' => 0]));
 
         $money = new Money(150, 'ABC');
 
         static::assertSame(150, $money->getMajorUnits());
-    }
-
-    public function test_it_can_be_converted_into_string(): void
-    {
-        static::assertSame("UAH 1.00", (string) new Money(100));
     }
 
     public function test_it_can_be_serialized_to_json(): void
