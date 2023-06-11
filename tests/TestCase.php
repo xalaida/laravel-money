79c47fc09f5cadf7e9a2b070415d2ffe9ba8eaed
nevadskiy <nevadskiy@gmail.com>
2023-06-11 19:31:03 +0300

    Add testing database connection

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Nevadskiy\Money\Tests;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Builder;
 use Illuminate\Foundation\Application;
 use Nevadskiy\Money\MoneyServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
@@ -18,5 +20,30 @@ abstract class TestCase extends OrchestraTestCase
         return [
             MoneyServiceProvider::class
         ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    /**
+     * Get a schema builder instance.
+     */
+    protected function schema(): Builder
+    {
+        return Model::getConnectionResolver()
+            ->connection()
+            ->getSchemaBuilder();
     }
 }
