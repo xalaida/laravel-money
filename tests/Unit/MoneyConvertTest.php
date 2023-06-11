76f8167660ac336117facb13cc4dc0eba896fc43
nevadskiy <nevadskiy@gmail.com>
2023-06-11 18:59:26 +0300

    Fix

--- tests/Unit/MoneyConvertTest.php
+++ tests/Unit/MoneyConvertTest.php
@@ -2,23 +2,21 @@
 
 namespace Nevadskiy\Money\Tests\Unit;
 
+use Nevadskiy\Money\Converter\ArrayConverter;
+use Nevadskiy\Money\Converter\Converter;
 use Nevadskiy\Money\Money;
-use Nevadskiy\Money\Registry\CurrencyRegistry;
+use Nevadskiy\Money\Scaler\ArrayScaler;
+use Nevadskiy\Money\Scaler\Scaler;
 use Nevadskiy\Money\Tests\TestCase;
 
 class MoneyConvertTest extends TestCase
 {
     public function test_it_can_be_converted_to_another_currency(): void
     {
-        $this->app->get(CurrencyRegistry::class)->set('USD', [
-            'rate' => 1.0,
-            'scale' => 2,
-        ]);
-
-        $this->app->get(CurrencyRegistry::class)->set('UAH', [
-            'rate' => 36.916908,
-            'scale' => 2,
-        ]);
+        $this->app->instance(Converter::class, new ArrayConverter([
+            'USD' => 1.0,
+            'UAH' => 36.916908,
+        ]));
 
         $money = Money::fromMajorUnits(100, 'USD')->convert('UAH');
 
@@ -30,15 +28,10 @@ class MoneyConvertTest extends TestCase
     {
         Money::setDefaultCurrency('USD');
 
-        $this->app->get(CurrencyRegistry::class)->set('USD', [
-            'rate' => 1.0,
-            'scale' => 2,
-        ]);
-
-        $this->app->get(CurrencyRegistry::class)->set('UAH', [
-            'rate' => 36.916908,
-            'scale' => 2,
-        ]);
+        $this->app->instance(Converter::class, new ArrayConverter([
+            'USD' => 1.0,
+            'UAH' => 36.916908,
+        ]));
 
         $money = Money::fromMajorUnits(100, 'UAH')->convert();
 
@@ -48,15 +41,15 @@ class MoneyConvertTest extends TestCase
 
     public function test_it_can_be_converted_into_money_to_another_currency_with_big_scale(): void
     {
-        $this->app->get(CurrencyRegistry::class)->set('UAH', [
-            'rate' => 36.916908,
-            'scale' => 2,
-        ]);
+        $this->app->instance(Converter::class, new ArrayConverter([
+            'UAH' => 36.916908,
+            'BTC' => 1 / 25000,
+        ]));
 
-        $this->app->get(CurrencyRegistry::class)->set('BTC', [
-            'rate' => 1 / 25000,
-            'scale' => 8,
-        ]);
+        $this->app->instance(Scaler::class, new ArrayScaler([
+            'UAH' => 2,
+            'BTC' => 8,
+        ]));
 
         $money = Money::fromMajorUnits(1, 'BTC');
 
@@ -65,15 +58,15 @@ class MoneyConvertTest extends TestCase
 
     public function test_it_can_be_converted_to_another_currency_with_zero_scale(): void
     {
-        $this->app->get(CurrencyRegistry::class)->set('USD', [
-            'rate' => 1.0,
-            'scale' => 2,
-        ]);
+        $this->app->instance(Converter::class, new ArrayConverter([
+            'USD' => 1.0,
+            'JPY' => 139.395,
+        ]));
 
-        $this->app->get(CurrencyRegistry::class)->set('JPY', [
-            'rate' => 139.395,
-            'scale' => 0,
-        ]);
+        $this->app->instance(Scaler::class, new ArrayScaler([
+            'USD' => 2,
+            'JPY' => 0,
+        ]));
 
         $money = new Money(50, 'USD');
 
