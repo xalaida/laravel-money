f9b7080290494b6070a6c915ae0462cf35326061
nevadskiy <nevadskiy@gmail.com>
2023-06-11 19:28:54 +0300

    Remove unused code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,27 +2,12 @@
 
 namespace Nevadskiy\Money\Tests;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Schema\Builder;
 use Illuminate\Foundation\Application;
-use Nevadskiy\Money\Money;
 use Nevadskiy\Money\MoneyServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    /**
-     * Setup the test environment.
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->artisan('migrate', ['--database' => 'testbench'])->run();
-
-        Money::setDefaultCurrency('UAH');
-    }
-
     /**
      * Get package providers.
      *
@@ -30,31 +15,8 @@ abstract class TestCase extends OrchestraTestCase
      */
     protected function getPackageProviders($app): array
     {
-        return [MoneyServiceProvider::class];
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param Application $app
-     */
-    protected function getEnvironmentSetUp($app): void
-    {
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-    }
-
-    /**
-     * Get a schema builder instance.
-     */
-    protected function schema(): Builder
-    {
-        return Model::getConnectionResolver()
-            ->connection()
-            ->getSchemaBuilder();
+        return [
+            MoneyServiceProvider::class
+        ];
     }
 }
