07803824fcbd592c6f887d49799f31171621f34a
nevadskiy <nevadskiy@gmail.com>
2023-06-13 16:13:35 +0300

    WIP

--- config/money.php
+++ config/money.php
@@ -5,9 +5,30 @@ return [
 
     'rate_provider' => env('MONEY_RATE_PROVIDER', 'array'),
 
+    // @todo >>>
+
+    'fallback_currency' => 'USD', // Currency to fallback when impossible to convert...
+
     'rate_providers' => [
+        'cache' => [
+            'driver' => 'cache',
+            'provider' => 'open_exchange_rates',
+            'ttl' => 60 * 24
+        ],
+
+        'array' => [
+            'driver' => 'array',
+            'rates' => [
+                'USD' => 1.0,
+                'EUR' => 0.92515185,
+            ],
+        ],
+
         'open_exchange_rates' => [
             'app_id' => env('OPEN_EXCHANGE_RATE_APP_ID'),
+            'driver' => 'open_exchange_rates',
+            'cache' => true,
+            'ttl' => 60 * 24,
         ],
     ],
 ];
