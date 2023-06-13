9d966b0d48782e0a13a8897f8a16a5696a862414
nevadskiy <nevadskiy@gmail.com>
2023-06-13 19:54:56 +0300

    Refactor

--- src/Exceptions/CurrencyRateMissingException.php
+++ src/Exceptions/CurrencyRateMissingException.php
@@ -7,10 +7,28 @@ use RuntimeException;
 class CurrencyRateMissingException extends RuntimeException
 {
     /**
+     * The currency of the exception.
+     *
+     * @var string
+     */
+    protected $currency;
+
+    /**
      * Make a new exception instance for the given currency.
      */
     public static function for(string $currency): self
     {
-        return new static(sprintf("Rate is missing for currency [%s].", $currency));
+        $e = new static(sprintf("Rate is missing for currency [%s].", $currency));
+        $e->currency = $currency;
+
+        return $e;
+    }
+
+    /**
+     * Get the currency of the exception.
+     */
+    public function getCurrency(): string
+    {
+        return $this->currency;
     }
 }
